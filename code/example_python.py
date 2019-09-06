5a3e8366ed6173bef9cf4f1362419aeed10ae8aa
GoThere <topboyli@163.com>
2019-09-06 11:17:19 -0400

    add some comments

--- code/example_python.py
+++ code/example_python.py
@@ -11,7 +11,7 @@ print("Hello world!")
 # 2. Calculate pi
 
 
-
+# this part is good!!
 # 3. Square function
 # adapted from https://hbctraining.github.io/Intro-to-R/lessons/03_introR-functions-and-arguments.html#user-defined-functions
 # and https://www.r-bloggers.com/how-to-write-and-debug-an-r-function/
